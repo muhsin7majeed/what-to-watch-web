7b12aaf3b6b74f9ac067f7302eb63ec5a045fb2c
muhsin7majeed <muhsin7ak@gmail.com>
2025-03-27 12:20:29 +0000

    ofcourse somne more cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { Provider } from './components/ui/provider.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="*" element={<div>Not found</div>} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 );
