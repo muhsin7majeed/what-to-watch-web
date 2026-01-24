6bfc5d69aacef6988780f38d698f487b3828dc2e
Muhsin <muhsin7ak@gmail.com>
2026-01-24 23:42:01 +0530

    feat: update media carousal skeleton to match actual UI

--- client/src/components/media-carousal/media-carousal-skeleton.tsx
+++ client/src/components/media-carousal/media-carousal-skeleton.tsx
@@ -1,15 +1,19 @@
-import { Flex, Skeleton } from '@chakra-ui/react';
+import { Box, Flex, Skeleton } from '@chakra-ui/react';
 
 const MediaCarousalSkeleton = () => {
   return (
-    <Flex gap={4}>
-      <Skeleton height="300px" flex="0 0 200px" />
-      <Skeleton height="300px" flex="0 0 200px" />
-      <Skeleton height="300px" flex="0 0 200px" />
-      <Skeleton height="300px" flex="0 0 200px" />
-      <Skeleton height="300px" flex="0 0 200px" />
-      <Skeleton height="300px" flex="0 0 200px" />
-    </Flex>
+    <Box>
+      <Skeleton height="40px" width="300px" mb="4" />
+
+      <Flex gap={4} overflowX="auto">
+        <Skeleton height="300px" flex="0 0 200px" />
+        <Skeleton height="300px" flex="0 0 200px" />
+        <Skeleton height="300px" flex="0 0 200px" />
+        <Skeleton height="300px" flex="0 0 200px" />
+        <Skeleton height="300px" flex="0 0 200px" />
+        <Skeleton height="300px" flex="0 0 200px" />
+      </Flex>
+    </Box>
   );
 };
 
